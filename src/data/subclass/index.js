c77719f562a9237e286ad88bef89424e72b6481b
dsalexan <danilo.salexan@gmail.com>
2020-04-16 05:47:54 -0300

    .

--- src/data/subclass/index.js
+++ src/data/subclass/index.js
@@ -4,7 +4,11 @@ import angel_of_death from './angel_of_death'
 // monk
 import zoroaster from './zoroaster'
 
+// paladin
+import stormkin from './stormkin'
+
 export default {
   angel_of_death,
   zoroaster,
+  stormkin,
 }
